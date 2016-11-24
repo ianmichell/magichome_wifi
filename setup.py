0d4ae0fac49327af9b9e9ff504649b658326f7cb
Ian Michell <ianmichell@me.com>
2016-11-24 09:29:33 +0000

    added classifiers

--- setup.py
+++ setup.py
@@ -4,9 +4,24 @@ setup(
     name='magichome_wifi',
     version='0.1',
     packages=['magichome_wifi'],
-    url='',
+    url='https://github.com/thunderbird/magichome_wifi',
     license='APACHE 2.0',
     author='Ian Michell',
     author_email='ianmichell@icloud.com',
-    description='Generic MagicHome RGBWWCW LED Controller driver'
+    description='Generic MagicHome RGBWWCW LED Controller driver',
+    classifiers=[
+        'Development Status :: 0.1 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ' +
+        'Apache Version 2.0',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
